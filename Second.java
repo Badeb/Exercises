70e403d95d888038be3a75824de834f7c75f1196
Badeb <120341470+Badeb@users.noreply.github.com>
2023-10-24 17:11:48 +0300

    Week 4 - Lab 4 - Fibonacci (for)

--- Second.java
+++ Second.java
@@ -5,14 +5,16 @@ public class Second {
     Scanner sc = new Scanner(System.in);
     int first = 1;
     int second = 1;
-        do {
-            System.out.println(first);
-            int sum = first + second;
-            first = second;
-            second = sum;
+        for(int i =1;i<100;++i) {
+            if (first <= 100) {
+                System.out.println(first);
+                int sum = first + second;
+                first = second;
+                second = sum;
+            } else {
+                break;
+            }
         }
-        while (first<=100) ;
-
-
     }
 }
+
