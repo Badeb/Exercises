29635a2ca666a8c69ea483dfc819d43e83065a14
Badeb <120341470+Badeb@users.noreply.github.com>
2023-10-24 17:08:57 +0300

    Week 4 -Lab 4 - Fibonacci

--- Second.java
+++ Second.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
+
 public class Second {
-    public static void main (String [] args) {
-        Scanner sc = new Scanner (System.in);
-        int a = 0;
-        int b = 0;
-
-        System.out.println("Please enter numbers for dividing for a/b");
-        a = sc.nextInt();
-        b = sc.nextInt();
-
-        if (b==0) {
-            System.out.println("undefined");
-        }else  {
-           System.out.println((float)a/b);
+    public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+    int first = 1;
+    int second = 1;
+        do {
+            System.out.println(first);
+            int sum = first + second;
+            first = second;
+            second = sum;
         }
+        while (first<=100) ;
+
+
     }
 }
