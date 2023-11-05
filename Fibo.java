aad4443d080406495ffea4dd831cc1b21133b096
Badeb <120341470+Badeb@users.noreply.github.com>
2023-11-05 23:01:25 +0300

    Week 5 - Senario 3

---
+++ Fibo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static int Fib (int n ) {
+        if (n == 1 ) return 1;
+        if (n == 0) return 0;
+        return Fib(n - 1) + Fib(n - 2);
+    }
+    public static void main (String [] args) {
+        Scanner sc =new Scanner (System.in);
+        System.out.println("enter number ");
+        int n =sc.nextInt();
+        System.out.println("fibonacci terim = " + Fib(n));
+
+
+    }
+}
